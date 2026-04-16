04c56a62a0fd1a1554357301573c034a2b41ef12
glenrock168-beep <glenrock168@gmail.com>
2026-04-16 14:30:27 -0400

    Update astro.config.mjs
    setting the path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  site: 'https://glenrock168-beep.github.io/',
+  base: '/Glenrock/',
 });
